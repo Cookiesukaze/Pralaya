a39edc26ac9e2beca2196f5cf5f3172094d498d2
Cookiesukaze <1426381378@qq.com>
2024-10-16 15:55:37 +0800

    style init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      中文文字也实施效果
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
@@ -32,6 +32,7 @@ const count = ref(0)
     >.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="read-the-docs">中文文字也实施效果</div>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import './assets/font/font.css';
 import './style.css'
 import App from './App.vue'
 
